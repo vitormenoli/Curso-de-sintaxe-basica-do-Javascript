ab609cb561566954883bb557ab63ede6533aebca
Vitor Menoli <vitoragario@gmail.com>
2024-02-23 02:30:21 -0300

    Atualizando .gitignore e removendo arquivos do cache

--- Modulo-2/3-nomenclaturas-variaveis/index.js
+++
@@ -1,5 +0,0 @@
-// Fomarto camelCase
-
-let nomeDoUsuario = 'Vitor'
-let texto = 'abc'
-let idadeDoUsuario = 25
--- Modulo-4/2-indices/index.js
+++
@@ -1,4 +0,0 @@
-const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-console.log(letras[0]) // A
-console.log(letras[letras.length - 1])
