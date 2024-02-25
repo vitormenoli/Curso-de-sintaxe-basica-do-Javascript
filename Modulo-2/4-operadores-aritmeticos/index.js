3444342570348c78f437f44835c8a2f25839481d
Vitor Menoli <vitoragario@gmail.com>
2024-02-25 02:28:23 -0300

    Adicionando Modulo 2 ao curso

---
+++ Modulo-2/4-operadores-aritmeticos/index.js
@@ -0,0 +1,10 @@
+const numeroUm = 10
+const numeroDois = 5
+
+const contaAdicao = numeroUm + numeroDois
+const contaSubtracao = numeroUm - numeroDois
+const contaMultiplicacao = numeroUm * numeroDois
+const contaDivisao = numeroUm / numeroDois
+const contaModulo = numeroUm % numeroDois
+
+console.log(contaModulo)
