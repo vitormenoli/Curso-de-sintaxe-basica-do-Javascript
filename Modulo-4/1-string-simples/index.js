c1628b86f92c204ec1474e47f1ff4b852a570b1a
Vitor Menoli <vitoragario@gmail.com>
2024-02-27 12:26:22 -0300

    Adicionando o Modulo 4 ao curso

---
+++ Modulo-4/1-string-simples/index.js
@@ -0,0 +1,12 @@
+const stringSimples1 = 'Olá '
+const stringSimples2 = 'mundo!'
+
+const stringConcatenada = stringSimples1 + stringSimples2
+
+console.log(stringConcatenada)
+
+const nome = 'Vitor'
+
+// alert('Meu nome é ' + nome)
+
+console.log(stringConcatenada.length)
